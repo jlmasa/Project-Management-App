22c707d187406dd6b15a74ea9a5e6baa0a3ab75d
john masa <johnlorenz.masa@gmail.com>
2024-11-14 17:47:45 +0800

    initial commit

---
+++ src/app/dashboardWrapper.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import React from "react";
+import Navbar from "@/app/(components)/Navbar";
+import Sidebar from "@/app/(components)/Sidebar";
+import StoreProvider, { useAppSelector } from "./redux";
+
+function DashboardLayout({ children }: { children: React.ReactNode }) {
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapse,
+  );
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
+  React.useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  });
+
+  return (
+    <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
+      {/* sidebar */}
+      <Sidebar />
+      <main
+        className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg ${
+          isSidebarCollapsed ? "" : "md:pl-64"
+        }`}
+      >
+        {/* navbar */}
+        <Navbar />
+        {children}
+      </main>
+    </div>
+  );
+}
+
+const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <StoreProvider>
+      <DashboardLayout>{children}</DashboardLayout>
+    </StoreProvider>
+  );
+};
+
+export default DashboardWrapper;
---
+++ src/state/index.ts
@@ -0,0 +1,27 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+export interface initialStateTypes {
+  isSidebarCollapse: boolean;
+  isDarkMode: boolean;
+}
+
+const initialState: initialStateTypes = {
+  isSidebarCollapse: false,
+  isDarkMode: false,
+};
+
+export const globalSlice = createSlice({
+  name: "global",
+  initialState,
+  reducers: {
+    setIsSidebarCollapsed: (state, action: PayloadAction<boolean>) => {
+      state.isSidebarCollapse = action.payload;
+    },
+    setIsDarkMode: (state, action: PayloadAction<boolean>) => {
+      state.isDarkMode = action.payload;
+    },
+  },
+});
+
+export const { setIsSidebarCollapsed, setIsDarkMode } = globalSlice.actions;
+export default globalSlice.reducer;
